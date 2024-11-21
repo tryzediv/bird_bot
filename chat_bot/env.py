275e8f65276e2a7f6bea46362bd6127a85c13003
d.e.kharin <d.e.kharin@tmn3.etagi.com>
2024-11-21 09:57:49 +0500

    feature_005

--- chat_bot/env.py
+++
@@ -1,4 +0,0 @@
-TOKEN = ('vk1.a.T5mw5nMAKa6cNzevXWwR1MXtrO9C01UvocZVTKz85mZfc58CFmSCbIgX5mux340Zc7eULcQzJ5vTsX-sFy_Pf-'
-         'Ro0b5boZfxWAPX4nju5h0xT3w1Ch4NhVWC4rFN6951pGUjXxCyswyUyqxDtBD0WatWqDgRn1viy'
-         '6h14gZy8k5kvOZRuV0qmhSFJiVRoy-yq7MLUJzDhsxDC7tX4JjdvQ')
-GROUP_ID = '228311047'
